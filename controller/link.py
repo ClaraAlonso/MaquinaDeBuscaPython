bf9f7520a9457dda7abef187ace5e4e8bac151e8
ClaraAlonso <claraalonsoleao@gmail.com>
2019-06-16 17:54:29 -0300

    Estruturaçao

--- controller/link.py
+++ controller/link.py
@@ -1,10 +1,9 @@
-
 from flask import Flask, render_template, request, url_for, redirect
 
-from flask.ext.cache.sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqllite:///db.sqllite'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 
 db = SQLAlchemy(app)
 
@@ -14,11 +13,11 @@ class Link(db.Model):
     
     _id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     url = db.Column(db.String)
-    ultimaColeta = db.Column(db.String)
+    #ultimaColeta = db.Column(db.String)
     
     def __init__(self, url, ultimaColeta):
-        Self.url =url
-        self.ultimaColeta = ultimaColeta
+        self.url = url
+        #self.ultimaColeta = ultimaColeta
         
 db.create_all()
 
@@ -26,12 +25,23 @@ db.create_all()
 def link():
     return render_template("link.html")
 
-@app.route("/inserirLink", methods=['POST'])
+@app.route("/inserirLink", methods=['GET', 'POST'])
 def inserirLink():
     if request.method == "POST":
         url = request.form.get("url")
-        ultimaColeta = request.form.get("ultimaColeta")
+        #ultimaColeta = request.form.get("ultimaColeta")
+    
+        if url:
+            link = Link(url)
+            db.session.add(link)
+            db.session.commit()
 
+    return redirect(url_for("index"))
+
+@app.route("/listaLink")
+def lista():
+    link = Link.query.all()
+    return render_template("listaLink.html", link=link)
 
 if __name__ == '__main__':
      app.run(debug=True)

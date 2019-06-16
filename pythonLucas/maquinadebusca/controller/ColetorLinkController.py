6af9b0278aab0ba8092d0a2aa917de4f302567ed
mateushmdz <mateushmdz@gmail.com>
2019-06-16 16:31:19 -0300

    iniciando

--- pythonLucas/maquinadebusca/controller/ColetorLinkController.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, jsonify
-import requests
-from bs4 import BeautifulSoup
-app = Flask (__name__)
-
-@app.route ("/coletar_link", methods=['POST']) 
-def coletar_link (): 
-    return jsonify ({"resp": "Coletor de link iniciado através do método POST."})
